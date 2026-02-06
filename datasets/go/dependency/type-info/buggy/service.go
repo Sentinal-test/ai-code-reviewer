3bb3bdad941f5bf58285d7d3bfd8c95cb881fb7b
Tegveer Singh <tegisingh94@gmail.com>
2026-02-06 12:16:10 +0530

    fix: permanently remove datasets from git tracking

--- datasets/go/dependency/type-info/buggy/service.go
+++
@@ -1,8 +0,0 @@
-package service
-
-import "code-review/models"
-
-func (s *UserService) NotifyUser(u *models.User) {
-	// BUG: u.Email no longer exists in models/user.go
-	println("Notifying: " + u.Email)
-}
--- datasets/go/dependency/type-info/deps/models/user.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type User struct {
-	ID        int
-	EmailAddr string // Field renamed from Email to EmailAddr
-	Name      string
-}
